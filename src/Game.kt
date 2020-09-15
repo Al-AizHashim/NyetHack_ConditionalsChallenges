5828ed92619dd5095dd85f6eb40c8d9af21624e2
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2020-09-15 10:30:01 +0300

    Update Game.kt

--- src/Game.kt
+++ src/Game.kt
@@ -2,18 +2,22 @@ fun main() {
     var healthPoints = 100
     val playerName = "Madrigal"
     val isBlessed = true
-    when(healthPoints){
-        100-> println("$playerName is in excellent condition!")
-        in 90..99-> println("$playerName has a view scratches.")
-        in 75..89->{
-            if (isBlessed) {
-                println("$playerName has some minor wounds but is healing quite quickly")
-            } else { println("$playerName has some minor wounds.")
-            }
-        }
-        in 15..74-> println("$playerName looks pretty hurt")
-        in 0..14-> println("$playerName is in awful condition")
+    val isImmortal = false
+    var auraColor:String?=null
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ).toInt()
+    when(karma){
+        in 0..5->  auraColor="red"
+        in 6..10->  auraColor="oringe"
+        in 11..15->  auraColor="purple"
+        in 16..20->  auraColor="green"
     }
-    println("The plyer name is: $playerName and the health points is:$healthPoints")
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    if (auraVisible) {
+        println("The aura color is: $auraColor ")
+    } else {
+        println("The aura color is: NONE")
+    }
+
+
 
 }
